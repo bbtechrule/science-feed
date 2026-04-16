4b55c4540bebad568d9e12e11ab690c26d888f76
bbtechrule <bbtechrule@gmail.com>
2026-04-16 06:38:46 +0530

    Update images.js

--- images.js
+++ images.js
@@ -1,1 +1,22 @@
+// images.js
 
+const posts = [
+  {
+    id:1,
+    img:"https://res.cloudinary.com/dujoufris/image/upload/v1769152609/cell_1.png_pfomgd.jpg",
+    title:"Cells 🔬",
+    desc:"Basic unit of life."
+  },
+  {
+    id:2,
+    img:"https://picsum.photos/600/1000?random=2",
+    title:"DNA 🧬",
+    desc:"Genetic material."
+  },
+  {
+    id:3,
+    img:"https://picsum.photos/600/1000?random=3",
+    title:"Brain 🧠",
+    desc:"Controls body."
+  }
+];
