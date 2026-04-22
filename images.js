a5730ebbd5894da188085cddcb2de72f9a0b96a5
bbtechrule <bbtechrule@gmail.com>
2026-04-22 12:58:54 +0530

    Update images.js

--- images.js
+++ images.js
@@ -2,5 +2,5 @@ const posts = [
   "https://res.cloudinary.com/demo/image/upload/sample.jpg",
   "https://picsum.photos/600/1000?random=1",
   "https://picsum.photos/600/1000?random=2",
-  "https://res.cloudinary.com/dujoufris/raw/upload/v1776756005/Here_are_some_important_health_benefits_you_should_know_simple_a..._obagsk.txt"
+  "https://res.cloudinary.com/dujoufris/image/upload/v1769152609/cell_1.png_pfomgd.jpg"
 ];
