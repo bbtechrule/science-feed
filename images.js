ca418390d2920cd63e93159283902bbb93cc86ba
bbtechrule <bbtechrule@gmail.com>
2026-04-22 13:43:26 +0530

    Remove example image URLs from posts array
    Removed example image URLs from the posts array.

--- images.js
+++ images.js
@@ -1,6 +1,3 @@
 const posts = [
-  "https://res.cloudinary.com/demo/image/upload/sample.jpg",
-  "https://picsum.photos/600/1000?random=1",
-  "https://picsum.photos/600/1000?random=2",
   "https://res.cloudinary.com/dujoufris/image/upload/v1769152609/cell_1.png_pfomgd.jpg"
 ];
