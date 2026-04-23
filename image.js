6af88cf56384d07372b9ef813a538881bb763c45
bbtechrule <bbtechrule@gmail.com>
2026-04-23 18:46:05 +0530

    Add image display with like and share buttons

---
+++ image.js
@@ -0,0 +1,50 @@
+const images = [
+  "https://res.cloudinary.com/demo/image/upload/sample.jpg",
+  "https://res.cloudinary.com/demo/image/upload/sample.jpg",
+  "https://res.cloudinary.com/demo/image/upload/sample.jpg"
+];
+
+const container = document.getElementById("post-container");
+
+images.forEach((url) => {
+  const post = document.createElement("div");
+  post.className = "post";
+
+  const img = document.createElement("img");
+  img.src = url;
+  img.className = "post-img";
+
+  const buttons = document.createElement("div");
+  buttons.className = "left-buttons";
+
+  const likeBtn = document.createElement("button");
+  likeBtn.innerHTML = "❤️";
+
+  let liked = false;
+  likeBtn.onclick = () => {
+    liked = !liked;
+    likeBtn.innerHTML = liked ? "💖" : "❤️";
+  };
+
+  const shareBtn = document.createElement("button");
+  shareBtn.innerHTML = "🔗";
+
+  shareBtn.onclick = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: "Check this",
+        url: url
+      });
+    } else {
+      alert("Share not supported");
+    }
+  };
+
+  buttons.appendChild(likeBtn);
+  buttons.appendChild(shareBtn);
+
+  post.appendChild(img);
+  post.appendChild(buttons);
+
+  container.appendChild(post);
+});
